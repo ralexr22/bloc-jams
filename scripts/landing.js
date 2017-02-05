56df3f60483016037176fc7c62a1e547f2fcb9ca
Alex Ramirez <r.alexramirez@outlook.com>
2017-02-05 15:47:38 -0500

    checkpoint 22 assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -28,3 +28,14 @@ var animatePoints = function() {
      revealThirdPoint();
  
  };
+
+var revealPoint = function(index) {
+     points[index].style.opacity = 1;
+     points[index].style.transform = "scaleX(1) translateY(0)";
+     points[index].style.msTransform = "scaleX(1) translateY(0)";
+     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";  
+    
+    for (var i = 0; i > index.length; i++)
+        revealPoint(index);
+    }
+};

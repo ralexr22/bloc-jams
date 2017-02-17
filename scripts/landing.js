4823b1acbc5db285fad5c35206e40e5c46bc673a
Alex Ramirez <r.alexramirez@outlook.com>
2017-02-16 23:42:53 -0500

    checkpoint 23 assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
 var pointsArray = document.getElementsByClassName('point');
 
  var animatePoints = function(points) {
-
     var revealPoint = function(index) {
          points[index].style.opacity = 1;
          points[index].style.transform = "scaleX(1) translateY(0)";
@@ -9,22 +8,20 @@ var pointsArray = document.getElementsByClassName('point');
          points[index].style.WebkitTransform = "scaleX(1) translateY(0)"; 
     };
 
-        animatePoints.forEach(revealPoint);
-      }
+    forEach(pointsArray, revealPoint);
+};
 
-     };
+window.onload = function() {
+    if (window.innerHeight > 950){
+        animatePoints(pointsArray);
+    }
 
-    window.onload = function() {
-        if (window.innerHeight > 950){
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
             animatePoints(pointsArray);
         }
-
-        var sellingPoints = document.getElementsByClassName('selling-points')[0];
-        var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-        window.addEventListener('scroll', function(event) {
-            if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-                animatePoints(pointsArray);
-            }
-        });
-    }
+    });
+}

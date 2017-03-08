489861d6f192809fa36f0e30f62641491c77b46e
Alex Ramirez <r.alexramirez@outlook.com>
2017-03-07 22:05:09 -0500

    Checkpoint 26

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,14 @@
-var animatePoints = function() {
- 
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
+ var animatePoints = function(points) {
     var revealPoint = function(index) {
-     points[index].style.opacity = 1;
-     points[index].style.transform = "scaleX(1) translateY(0)";
-     points[index].style.msTransform = "scaleX(1) translateY(0)";
-     points[index].style.WebkitTransform = "scaleX(1) translateY(0)"; 
-};
-    
-    for (var i = 0; i < points.length; i++){
-        revealPoint(i);
-  }
+         points[index].style.opacity = 1;
+         points[index].style.transform = "scaleX(1) translateY(0)";
+         points[index].style.msTransform = "scaleX(1) translateY(0)";
+         points[index].style.WebkitTransform = "scaleX(1) translateY(0)"; 
+    };
+
+    forEach(pointsArray, revealPoint);
 };
 
 window.onload = function() {
